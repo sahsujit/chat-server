3026017f3f913424e4cfefee2d63e477ab5db52d
sujit sah <sujit8877123sah@gmail.com>
2024-11-12 16:31:44 +0545

    setting up socket.io

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,8 @@ const userController = require("../controllers/user")
 const authController = require("../controllers/auth")
 
 router.patch("/update-me",authController.protect ,userController.updateMe)
+router.get("/get-users", authController.protect, userController.getUsers);
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,45 +1,78 @@
 const { default: mongoose } = require("mongoose");
 const app = require("./app");
 
-process.on("uncaughtException",(err)=>{
-    console.log(err);
-    process.exit(1);
-})
+const { Server } = require("socket.io");
 
-
-
-
-
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  process.exit(1);
+});
 
 const http = require("http");
-
+const User = require("./models/user");
+const FriendRequest = require("./models/friendRequest");
 
 const server = http.createServer(app);
 require("dotenv").config();
 
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
 const DB = process.env.MONGODB_URL;
 // console.log("db", DB)
 
-mongoose.connect(DB,{
-   
+mongoose
+  .connect(DB, {})
+  .then(() => {
+    console.log("DB connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-}).then(()=>{
-    console.log("DB connected successfully")
-}).catch((err)=>{
-    console.log(err)
-})
+const port = process.env.PORT || 8000;
 
-const port = process.env.PORT || 8000
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
+io.on("connection", async (socket) => {
+  const user_id = socket.handshake.query["user_id"];
 
-server.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-})
+  if (user_id !== null && Boolean(user_id)) {
+    try {
+      User.findByIdAndUpdate(user_id, {
+        socket_id: socket.id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-process.on("unhandledRejection",()=>{
-   
-    server.close(()=>{
-        process.exit(1);
-    })
-})
+  socket.on("friend_request", async (data) => {
+    const to = await User.findById(data.to).select("socket_id");
+    const from = await User.findById(data.from).select("socket_id");
 
+    await FriendRequest.create({
+      sender: data.from,
+      recipient: data.to,
+    });
+
+    io.to(to?.socket_id).emit("new_friend_request", {
+      message: "New friend request received",
+    });
+    io.to(from?.socket_id).emit("request_sent", {
+      message: "Request Sent successfully!",
+    });
+  });
+});
+
+process.on("unhandledRejection", () => {
+  server.close(() => {
+    process.exit(1);
+  });
+});
